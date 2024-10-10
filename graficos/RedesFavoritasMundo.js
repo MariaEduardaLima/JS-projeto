9b2d7ad2c2d7bbfaeb092375e6c43f907c92dab9
Maria Eduarda <106595568+MariaEduardaLima@users.noreply.github.com>
2024-10-10 20:06:31 -0300

    .

--- graficos/RedesFavoritasMundo.js
+++ graficos/RedesFavoritasMundo.js
@@ -1,5 +1,7 @@
+import { getCSS } from "./common.js"
+
 async function RedesFavoritasMundo() {
-    const url =""
+    const url ="https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json"
     const res = await fetch (url)
     const dados = await res.json () 
     const redes = Object.keys(dados)
@@ -14,7 +16,31 @@ async function RedesFavoritasMundo() {
         }
     ]
 
-    
+    const laytout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Redes sociais que os usuários mais gostam',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30,
+            }
+        },
+       legend: {
+        font: {
+            color: getCSS('--primary-color'),
+            size: 16
+         }
+       }
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, laytout)
+
 }
 
 RedesFavoritasMundo()
