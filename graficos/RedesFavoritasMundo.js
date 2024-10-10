edef809af2c79c6d3eee729e961d3241c9386d8e
Maria Eduarda <106595568+MariaEduardaLima@users.noreply.github.com>
2024-10-10 19:48:39 -0300

    .

--- graficos/RedesFavoritasMundo.js
+++ graficos/RedesFavoritasMundo.js
@@ -1,3 +1,18 @@
-async function RedesFavoritasMundos() {
-    const url=""
+async function RedesFavoritasMundo() {
+    const url =""
+    const res = await fetch (url)
+    const dados = await res.json () 
+    const redes = Object.keys(dados)
+    const valores = Object.values(dados)
+
+    const data = [
+        {
+            values: valores,
+            labels: redes,
+            type: "pie"
+            textinfo: "label+percent"
+        }
+    ]
 }
+
+RedesFavoritasMundo()
